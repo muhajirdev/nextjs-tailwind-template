efa146c9015b71a25b1595eaa28e9c3a895d21d3
Muhammad Muhajir <muhajirframe@gmail.com>
2019-07-13 22:04:11 +0700

    style: logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 const Home = () => (
   <Layout>
     <h1 className="bg-black text-white h-screen flex justify-center items-center">
-      NEXTJS + TAILWINDCSS = ❤️
+      NextJS + TailwindCSS + Typescript = ❤️
     </h1>
   </Layout>
 );
