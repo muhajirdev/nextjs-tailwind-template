656ee27790fef566c73db0fd1ad781cd6015c68f
Muhammad Muhajir <muhajirframe@gmail.com>
2019-07-31 19:58:42 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 const Home = () => (
   <Layout>
     <div className="bg-black text-white h-screen flex flex-col justify-center items-center">
-      <h1 className="text-2xl">YukNgaji Jaksel</h1>
+      <h1 className="text-2xl">Next Tailwind Typescript</h1>
       <h2>Coming Soon</h2>
     </div>
   </Layout>
